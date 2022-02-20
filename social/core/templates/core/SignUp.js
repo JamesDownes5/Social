afa66d3563fb4f6d2d7db96ce23d90adefb97a80
c43480ct <charles.tsang@student.manchester.ac.uk>
2022-02-20 17:20:24 +0000

    Login and signup version 1 with CSS file

---
+++ social/core/templates/core/SignUp.js
@@ -0,0 +1,9 @@
+const signup = Vue.createApp({
+    data() {
+        return{
+            logo: '/home/csimage/COMP10120_website/social/core/static/core/logo2.png',
+        }
+    }
+})
+
+signup.mount('#SignUp')
