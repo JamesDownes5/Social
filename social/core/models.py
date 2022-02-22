f646192ad6eccadfa0dfb5d6f49911f8e8d1934f
James Downes <james.downes@student.manchester.ac.uk>
2022-02-22 14:32:04 +0000

    Basic event model created and added dummy events

--- social/core/models.py
+++ social/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.test import tag
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=50)
+#    image = models.ImageField(upload_to="static/core/event/")
+    location = models.CharField(max_length=50)
+    date =  models.DateField()
+    desc = models.CharField(max_length=400)
+    ticket = models.URLField()
+    # social = 
+    # tag = 
+
