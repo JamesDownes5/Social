d482bfb4bc6a49b9c0ce88506921be108e1f76dd
c43480ct <charles.tsang@student.manchester.ac.uk>
2022-03-02 11:21:08 +0000

    Ready for merge

--- social/core/templates/core/SignUp.js
+++
@@ -1,9 +0,0 @@
-const signup = Vue.createApp({
-    data() {
-        return{
-            logo: '/home/csimage/COMP10120_website/social/core/static/core/logo2.png',
-        }
-    }
-})
-
-signup.mount('#SignUp')
