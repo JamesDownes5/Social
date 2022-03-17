603c4f250bbd802a5ae17f3223a8ad5b5d20453e
Felix Waller <felix.waller@student.manchester.ac.uk>
2022-03-17 16:13:07 +0000

    Merge branch 'User' into HEAD

--- social/core/admin.py
+++ social/core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Event, Profile
+from .models import Event, Profile, Friend_Request
 
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(Event)
+admin.site.register(Friend_Request)
