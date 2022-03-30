7fc5f71e5616919b7ccd4ef9259df78bbed73e8f
Felix Waller <felix.waller@student.manchester.ac.uk>
2022-03-30 21:35:43 +0100

    Add tags

--- social/core/static/core/event.js
+++ social/core/static/core/event.js
@@ -8,6 +8,10 @@ if (is_interested == "on") {
     document.getElementById("interest-button").textContent = "Interested Registered"
 }
 
+document.querySelectorAll(".tag").forEach(tag => tag.addEventListener("click", e => {
+    window.location.href = window.location.origin + "?tag=" + e.target.textContent.substr(e.target.textContent.indexOf(" ") + 1)
+}));
+
 document.getElementById("copy-button").addEventListener("click", () => {
     console.log("hey");
     var inputc = document.body.appendChild(document.createElement("input"));
