cdc6bb0ba56b7f3dfb261cb9be37b5743139b5da
Felix Waller <felix.waller@student.manchester.ac.uk>
2022-03-30 21:12:29 +0100

    Add interest registered effect

--- social/core/static/core/event.js
+++ social/core/static/core/event.js
@@ -1,5 +1,13 @@
 document.getElementById("event-img").style.height = document.getElementById("event-container").offsetHeight + "px";
 
+let url = new URL(window.location.href);
+let is_interested = url.searchParams.get("interested");
+
+if (is_interested == "on") {
+    document.getElementById("interested").checked = false;
+    document.getElementById("interest-button").textContent = "Interested Registered"
+}
+
 document.getElementById("copy-button").addEventListener("click", () => {
     console.log("hey");
     var inputc = document.body.appendChild(document.createElement("input"));
