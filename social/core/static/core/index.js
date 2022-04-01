60400b178259084ca3677d7bbe936de84f55f23f
Felix Waller <felix.waller@student.manchester.ac.uk>
2022-04-01 12:54:18 +0100

    Fix event redirecting

--- social/core/static/core/index.js
+++ social/core/static/core/index.js
@@ -33,19 +33,6 @@ if (search_field) {
     document.querySelector(".search-form").scrollIntoView();
 }
 
-// Event links
-document.querySelectorAll(".card").forEach(element => {
-    element.addEventListener("click", e => {
-        if (!e.target.classList.contains("tag")) {
-            window.location.href = origin + element.getAttribute("data-link");            
-        }
-    });
-});
-
-document.querySelectorAll(".tag").forEach(tag => tag.addEventListener("click", e => {
-    window.location.href = window.location.origin + "?search=" + e.target.textContent.replace("#", "%23");
-}));
-
 function showSlide(n) {
     let slide_index = n;
     let slideshow = document.querySelectorAll(".slideshow");
